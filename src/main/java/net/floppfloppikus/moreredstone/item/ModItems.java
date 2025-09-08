d617dc645843a12cc3ca4daa7ed3560ce75c8f1f
FloppFloppikus <floppagood0507@gmail.com>
2025-09-08 21:09:32 +0700

    Register added

--- src/main/java/net/floppfloppikus/moreredstone/item/ModItems.java
+++ src/main/java/net/floppfloppikus/moreredstone/item/ModItems.java
@@ -2,6 +2,7 @@ package net.floppfloppikus.moreredstone.item;
 
 import net.floppfloppikus.moreredstone.MoreRedstone;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -9,4 +10,7 @@ public class ModItems {
     public static final DeferredRegister<Item> Items =
             DeferredRegister.create(ForgeRegistries.ITEMS, MoreRedstone.MOD_ID);
 
+    public static void register(IEventBus EventBus){
+        Items.register(EventBus);
+    }
 }
